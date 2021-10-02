2bd07cbd8a7c83dacf62887e44b6888b7f7aadd0
Aaron Elizondo <acelizondo11@gmail.com>
2021-10-02 10:35:21 -0500

    Adding simle flash errors to hadle missing inputs and errors from flickr api

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,16 @@ class StaticPagesController < ApplicationController
 
         unless params[:flickr_id].blank?
             puts params[:flickr_id]
-            @pictures_list = flickr.people.getPublicPhotos :user_id => params[:flickr_id]
+            begin
+                @pictures_list = flickr.people.getPublicPhotos :user_id => params[:flickr_id]
+            rescue => e
+                message = e.message.split("-")
+                flash[:alert] = message[1] + " - Please Try Again"
+            end
+        else
+            if params.has_key?(:flickr_id)
+                flash[:alert] = "Please Enter a Valid User ID" 
+            end
         end
     end
 
