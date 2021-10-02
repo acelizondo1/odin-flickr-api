b532c70d95873568fa66dd7d3f8c73babf93202e
Aaron Elizondo <acelizondo11@gmail.com>
2021-10-02 08:59:29 -0500

    Adding flickr_api call loading to page load if correct user_id is enered

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,10 +4,9 @@ class StaticPagesController < ApplicationController
     def index
         flickr = Flickr.new "38df1bf08bb5c17a7702895b4572e302", "a8e7cdaf7685b20f"
 
-        if params[:flickr_id].blank?
-            #@lists = flickr.photos.getRecent
-        else
-
+        unless params[:flickr_id].blank?
+            puts params[:flickr_id]
+            @pictures_list = flickr.people.getPublicPhotos :user_id => params[:flickr_id]
         end
     end
 
