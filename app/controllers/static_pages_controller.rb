7ab3cd3fa6ad61326b2c6ad3d17a0f6b99e82f47
Aaron Elizondo <acelizondo11@gmail.com>
2021-10-03 10:03:03 -0500

    Adding username functionality, allows users to search by flickr id or username

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,13 +5,19 @@ class StaticPagesController < ApplicationController
         flickr = Flickr.new "38df1bf08bb5c17a7702895b4572e302", "a8e7cdaf7685b20f"
 
         unless params[:flickr_id].blank?
-            puts params[:flickr_id]
-            begin
-                @pictures_list = flickr.people.getPublicPhotos :user_id => params[:flickr_id]
-                @user = flickr.people.getInfo :user_id => params[:flickr_id]
-            rescue => e
-                message = e.message.split("-")
-                flash[:alert] = message[1] + " - Please Try Again"
+            begin 
+                user = flickr.people.findByUsername :username => params[:flickr_id]
+                flickr_id = user.id
+            rescue
+                flickr_id = params[:flickr_id]
+            ensure
+                begin
+                    @pictures_list = flickr.people.getPublicPhotos :user_id => flickr_id
+                    @user = flickr.people.getInfo :user_id => flickr_id
+                rescue => e
+                    message = e.message.split("-")
+                    flash[:alert] = message[1] + " - Please Try Again"
+                end
             end
         else
             if params.has_key?(:flickr_id)
